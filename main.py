176f746dc3a1715fd098847788c7bee2874b042f
Li, Casey <zl42@illinois.edu>
2021-12-11 14:45:00 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+
+
+threshold = 0.5  # Threshold to detect object
+#
+cap = cv2.VideoCapture(1)
+cap.set(3, 1280)
+cap.set(4, 720)
+#cap.set(10,70) #Brightness of the camera
+
+
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().split('\n')
+    print(classNames)
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while True:
+    success, img = cap.read()
+    classIds, confidence, boundingBox = net.detect(img, confThreshold=threshold)
+    print(classIds, boundingBox)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confidence.flatten(), boundingBox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classId - 1], (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1,
+                        (0, 255, 0), 2)
+
+    cv2.imshow("Video", img)
+    cv2.waitKey(1)
