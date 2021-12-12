7796515dd00ab0bbeb9218bc2b0bb17bbb3da944
Li, Casey <zl42@illinois.edu>
2021-12-11 22:18:35 -0600

    bug fixed

--- MainModule.py
+++ MainModule.py
@@ -1,11 +1,11 @@
 from ObjectDetectionForPi import *
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 #cap.set(10,70) #Brightness of the camera
 while True:
     success, img = cap.read()
-    objectIdentified, objectInfo = getObject(img)
+    objectIdentified, objectInfo = getObject(img, objects=['mouse'])
     cv2.imshow("Output", img)
     cv2.waitKey(1)
